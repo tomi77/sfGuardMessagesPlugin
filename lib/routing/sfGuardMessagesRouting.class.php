a37dc110a2603487d4ee406e80eeeb775260a5ad
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2017-10-05 21:04:02 +0200

    StyleCI fixes

--- lib/routing/sfGuardMessagesRouting.class.php
+++ lib/routing/sfGuardMessagesRouting.class.php
@@ -1,7 +1,6 @@
 <?php
 
 /**
- *
  * @author Tomasz Jakub Rup <tomasz.rup@gmail.com>
  */
 class sfGuardMessagesRouting
@@ -9,36 +8,36 @@ class sfGuardMessagesRouting
     public static function addRouteForAdminMessage(sfEvent $event)
     {
         $event->getSubject()->prependRoute('sf_guard_message', new sfPropelRouteCollection([
-      'name'                 => 'sf_guard_message',
-      'model'                => 'sfGuardMessage',
-      'module'               => 'sfGuardMessage',
-      'prefix_path'          => 'sf_guard_message',
+      'name' => 'sf_guard_message',
+      'model' => 'sfGuardMessage',
+      'module' => 'sfGuardMessage',
+      'prefix_path' => 'sf_guard_message',
       'with_wildcard_routes' => true,
-      'requirements'         => [],
+      'requirements' => [],
     ]));
     }
 
     public static function addRouteForAdminMessagePriority(sfEvent $event)
     {
         $event->getSubject()->prependRoute('sf_guard_message_priority', new sfPropelRouteCollection([
-      'name'                 => 'sf_guard_message_priority',
-      'model'                => 'sfGuardMessagePriority',
-      'module'               => 'sfGuardMessagePriority',
-      'prefix_path'          => 'sf_guard_message_priority',
+      'name' => 'sf_guard_message_priority',
+      'model' => 'sfGuardMessagePriority',
+      'module' => 'sfGuardMessagePriority',
+      'prefix_path' => 'sf_guard_message_priority',
       'with_wildcard_routes' => true,
-      'requirements'         => [],
+      'requirements' => [],
     ]));
     }
 
     public static function addRouteForAdminMessageType(sfEvent $event)
     {
         $event->getSubject()->prependRoute('sf_guard_message_type', new sfPropelRouteCollection([
-      'name'                 => 'sf_guard_message_type',
-      'model'                => 'sfGuardMessageType',
-      'module'               => 'sfGuardMessageType',
-      'prefix_path'          => 'sf_guard_message_type',
+      'name' => 'sf_guard_message_type',
+      'model' => 'sfGuardMessageType',
+      'module' => 'sfGuardMessageType',
+      'prefix_path' => 'sf_guard_message_type',
       'with_wildcard_routes' => true,
-      'requirements'         => [],
+      'requirements' => [],
     ]));
     }
 }
